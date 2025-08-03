0cd5c1af68df769870106826bf60273c3405be9f
aks-heev <aksheevs@gmail.com>
2025-08-03 18:22:10 +0530

    added node.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist'
+  },
   define: {
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
     'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
   }
+  // any other config
 })
