02d0529770924b124e1cf7f974f0e96f9f2cb05d
aks-heev <aksheevs@gmail.com>
2025-08-09 20:59:43 +0530

    changes in vite file to include /admin as second entry point

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,17 @@
 // vite.config.mjs
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+      }
+    }
   },
   define: {
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
